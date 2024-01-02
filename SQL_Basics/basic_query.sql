bf5288b813590fa6538ab06b11a9e6e01998f4a6
RAJAPAUL <61533823+M-Raj123@users.noreply.github.com>
2024-01-02 22:31:54 +0530

    basic query

---
+++ SQL_Basics/basic_query.sql
@@ -0,0 +1,14 @@
+-- show existing databases in ascending order
+show databases; 
+
+-- create db 
+create database Student;
+
+-- delete db 
+drop database Student;
+
+-- using a database
+use Student; 
+
+/* Multi-Line Comment */
+-- Single-Line comment
