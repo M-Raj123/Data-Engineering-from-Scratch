4c9feb3ac02c664a40a0164c25703d9b1b1d286b
RAJAPAUL <61533823+M-Raj123@users.noreply.github.com>
2024-01-13 23:45:15 +0530

    added where

--- SQL_Basics/DQL_Command.sql
+++ SQL_Basics/DQL_Command.sql
@@ -6,3 +6,6 @@ Select * from Orders limit 5;
 
 -- OFFSET - number of rows to skip from the beginning of the returned data before presenting the results
 Select * from Orders limit 5 offset 3;
+
+-- where ( used to extract only those records that fulfill a specified condition )
+select * from Item where Item_Id = 2 ;
