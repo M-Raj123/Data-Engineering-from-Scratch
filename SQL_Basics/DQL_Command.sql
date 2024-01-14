338c51028effcb4e4f7b09841e971d1755313dbe
RAJAPAUL <61533823+M-Raj123@users.noreply.github.com>
2024-01-14 23:12:39 +0530

    Added NULL Functions

--- SQL_Basics/DQL_Command.sql
+++ SQL_Basics/DQL_Command.sql
@@ -9,3 +9,10 @@ Select * from Orders limit 5 offset 3;
 
 -- where ( used to extract only those records that fulfill a specified condition )
 select * from Item where Item_Id = 2 ;
+
+-- if stock is null the result can be also return null value
+select Item_Id , Item_Name, (stock*Item_Price) as Amount from Item;
+
+-- IFNULL and COALESCE - returns 0 if the value is NULL:
+select Item_Id , Item_Name, (IFNULL(stock,0) * Item_Price) as Amount from Item;
+select Item_Id , Item_Name, (COALESCE(stock,0) * Item_Price) as Amount from Item; 
