6607b5fa42af5e62441b0d544eb37237cdeca622
RAJAPAUL <61533823+M-Raj123@users.noreply.github.com>
2024-02-08 23:29:11 +0530

    String Manipulation

---
+++ Python/Basics1.py
@@ -0,0 +1,22 @@
+print("Hello world");
+
+string = "Hello world"
+print(string)
+print(string[2::-1])
+
+s1 = "Hello"
+s2 = "world"
+print(s1+' '+s2)
+print('-'*10)
+
+print(string[8])   # index
+print(string[2::]) # slicing
+print(string[-6:]) 
+print("world" in string)
+
+
+print(string.upper())
+print(string.lower())
+
+print("he is boy and age %s and %s " % (10,"raj"))
+print("he is boy and age {} and {} ".format("00","raj"))
