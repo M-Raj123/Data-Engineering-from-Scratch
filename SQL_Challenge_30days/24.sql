41d47df2634629d4610654d47236db356e87cf97
RAJAPAUL <61533823+M-Raj123@users.noreply.github.com>
2024-10-04 23:26:25 +0530

    Q24

---
+++ SQL_Challenge_30days/24.sql
@@ -0,0 +1,46 @@
+/*
+
+Consider a table named customers with the following columns: 
+customer_id, first_name, last_name, and email. 
+Write an SQL query to find all the duplicate email addresses 
+in the customers table.
+
+*/
+
+-- Creating the customers table
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100)
+);
+
+-- Inserting sample data into the customers table
+INSERT INTO customers (customer_id, first_name, last_name, email) VALUES
+(1, 'John', 'Doe', 'john.doe@example.com'),
+(2, 'Jane', 'Smith', 'jane.smith@example.com'),
+(3, 'Alice', 'Johnson', 'alice.johnson@example.com'),
+(4, 'Bob', 'Brown', 'bob.brown@example.com'),
+(5, 'Emily', 'Davis', 'john.doe@example.com'),
+(6, 'Michael', 'Williams', 'michael.w@example.com'),
+(7, 'David', 'Wilson', 'jane.smith@example.com'),
+(8, 'Sarah', 'Taylor', 'sarah.t@example.com'),
+(9, 'James', 'Anderson', 'james.a@example.com'),
+(10, 'Laura', 'Martinez', 'laura.m@example.com');
+
+
+
+SELECT 
+  email , 
+  count(email) as freq
+FROM customers
+GROUP BY email 
+HAVING count(email) > 1 ;
+
+-- (or)
+
+SELECT c1.email
+FROM customers c1
+JOIN customers c2 ON c1.email = c2.email
+WHERE c1.customer_id <> c2.customer_id
+GROUP BY c1.email;
