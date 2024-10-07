05d7cbd9d208cb36bd5c102982baae1ffaa0d983
RAJAPAUL <61533823+M-Raj123@users.noreply.github.com>
2024-10-07 22:54:41 +0530

    Q27

---
+++ SQL_Challenge_30days/27.sql
@@ -0,0 +1,36 @@
+-- Flipkart Data Analyst Interview Questions
+-- Question: Write an SQL query to fetch user IDs that have only bought both 'Burger' and 'Cold Drink' items.
+
+-- Expected Output Columns: user_id
+
+-- Create the orders table
+CREATE TABLE orders (
+    user_id INT,
+    item_ordered VARCHAR(512)
+);
+
+-- Insert sample data into the orders table
+INSERT INTO orders VALUES 
+('1', 'Pizza'),
+('1', 'Burger'),
+('2', 'Cold Drink'),
+('2', 'Burger'),
+('3', 'Burger'),
+('3', 'Cold Drink'),
+('4', 'Pizza'),
+('4', 'Cold Drink'),
+('5', 'Cold Drink'),
+('6', 'Burger'),
+('6', 'Cold Drink'),
+('7', 'Pizza'),
+('8', 'Burger');
+
+
+
+SELECT 
+  user_id 
+FROM orders
+GROUP BY user_id 
+HAVING COUNT(DISTINCT item_ordered) = 2
+AND SUM(CASE WHEN item_ordered IN ('Burger', 'Cold Drink') 
+         THEN 1 ELSE 0 ) = 2
